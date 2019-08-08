d20a1188ae2a3880f19c52c9a8f2a1fef1d316fa
truongphan97gl <truongphan97gl@gmail.com>
2019-08-08 11:35:40 -0400

    Created comment model
    - Create with post ref
    - CRUD

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const postRoutes = require('./app/routes/post_routes')
+const commentRoutes = require('./app/routes/comment_routes')
+
 // require error handling middleware
 const errorHandler = require('./lib/error_handler')
 
@@ -61,6 +63,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(postRoutes)
+app.use(commentRoutes)
+
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them

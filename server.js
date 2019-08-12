31fd8d65e0f2ca6988e109ddea1922c310e89e2c
truongphan97gl <truongphan97gl@gmail.com>
2019-08-12 16:01:36 -0400

    Remove all the console.log

--- server.js
+++ server.js
@@ -71,9 +71,6 @@ app.use(commentRoutes)
 app.use(errorHandler)
 
 // run API on designated port (4741 in this case)
-app.listen(port, () => {
-  console.log('listening on port ' + port)
-})
 
 // needed for testing
 module.exports = app
